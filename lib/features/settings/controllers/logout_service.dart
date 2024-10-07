bd925088ff8285da799936dfd54133ca5639d1a1
nitin-tec-sense <nitin@tec-sense.com>
2024-10-07 13:30:33 +0530

    Launcher icons for ios and logout service.

--- lib/features/settings/controllers/logout_service.dart
+++ lib/features/settings/controllers/logout_service.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:page_animation_transition/animations/fade_animation_transition.dart';
+import 'package:page_animation_transition/page_animation_transition.dart';
+import 'package:swype/features/authentication/login/screens/onboarding/onboarding_screen.dart';
 import 'package:swype/features/authentication/providers/auth_provider.dart';
 import 'package:swype/features/authentication/providers/register_provider.dart';
 import 'package:swype/features/authentication/providers/user_provider.dart';
@@ -14,14 +17,18 @@ class LogoutService {
       final response = await dioClient.postWithoutData(ApiRoutes.logout);
       if (response.statusCode == 200) {
         if (response.data['status_code'] == 200) {
-          Navigator.pushNamedAndRemoveUntil(
+          Navigator.pushAndRemoveUntil(
             context,
-            '/login',
+            PageAnimationTransition(
+              page: const OnboardingScreen(),
+              pageAnimationType: FadeAnimationTransition(),
+            ),
             ModalRoute.withName('/'),
           );
           await ref.read(authProvider.notifier).logout();
           await ref.read(registerProvider.notifier).logout();
           ref.read(userProvider.notifier).clearUser();
+          
           return true;
         } else {
           return false;
